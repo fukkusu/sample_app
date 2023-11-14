802218735c1589f098b773c950db1eeac0dd3437
fukkusu <janette.fuchs@gtmi.co.jp>
2023-11-14 17:10:03 +0900

    Refactoring
    Addressing DRY:
    using <% yield %> for the body

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
   get 'static_pages/help'
   get 'static_pages/about'
-  get 'foo/bar'
-  get 'foo/baz'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "application#hello"
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'static_pages/contact'
 end
