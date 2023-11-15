d014a3f85dabdc27c4236839f0d91a5e3bb722de
fukkusu <janette.fuchs@gtmi.co.jp>
2023-11-15 04:19:11 +0000

    Complete advanced testing setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
    
 end
+# ActionController::Baseクラスとは
+# コントローラーの基本的な機能を提供するクラスです。
+# リクエスト／レスポンス処理に関わる基盤部分を担っています。
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -16,7 +16,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_help_url
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
-    
   end
 
   test "should get about" do
