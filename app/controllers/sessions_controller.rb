bcced64fbc567011370658f41a0187848fc95871
fukkusu <janette.fuchs@gtmi.co.jp>
2023-11-28 17:04:19 +0900

    Login page up
    login page flash.now[:danger]

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,15 @@
 class SessionsController < ApplicationController
   def new
   end
-  
+
   def create
-    render 'new', status: :unprocessable_entity
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new', status: :unprocessable_entity
+    end
   end
 
   def destroy
