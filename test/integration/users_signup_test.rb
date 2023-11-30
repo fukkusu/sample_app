7eca4a43a28ca46ace537512d3d4f4c4b7e34df3
fukkusu <janette.fuchs@gtmi.co.jp>
2023-11-30 11:12:47 +0900

    Update users_signup_test.rb
    test for "valid signup information"

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -15,9 +15,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     assert_select 'div.field_with_errors'
   end
-
-  test "signup information" do
-    get signup_path
+  test "valid signup information" do
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name:  "Example User",
                                          email: "user@example.com",
